56dc594c54143db3d8f1ef8db7ed7641137d96b4
Colin Megill <colinmegill@Colins-MacBook-Air.local>
2021-04-10 13:33:31 -0700

    worldview example, and docs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import WorldviewDemo from "./worldview-example";
 
 const App = () => {
-  return <div>from App rerender</div>;
+  return (
+    <div>
+      <p>from App rerender</p>
+      <p> now for a worldview demo! </p>
+
+      <div style={{ width: "50vw", height: "50vh" }}>
+        <WorldviewDemo />
+      </div>
+    </div>
+  );
 };
 
 export default App;

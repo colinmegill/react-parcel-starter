4b132bfd617686d485244d810a9acb5b5d320a2e
Colin Megill <colinmegill@gmail.com>
2021-04-19 01:45:06 -0700

    fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import WorldviewDemo from "./worldview-example";
-import { csv } from "d3-fetch";
+import { useFetch } from "./hooks/useFetch";
+
+// https://observablehq.com/@jermspeaks/async-await
 
 const App = () => {
-  csv(
+  const [data, loading] = useFetch(
     "https://raw.githubusercontent.com/colinmegill/react-parcel-starter/main/weather.csv"
-  ).then((data) => console.log(data));
+  );
+
+  console.log("from hook", loading, data);
   return (
     <div>
       <h1>Exploratory Data Analysis, Assignment 2, INFO 474 SP 2021</h1>
-      <p>Data!</p>
+      <p>{loading && "Loading data!"}</p>
     </div>
   );
 };
